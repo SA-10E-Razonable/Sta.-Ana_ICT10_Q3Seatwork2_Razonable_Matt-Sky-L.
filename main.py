3c813f4b6a58f4931fb82d63dab34640fda37489
Matt Sky L.Razonable <razonable.ml@obmontessori.edu.ph>
2026-02-04 19:00:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ from pyscript import display, document
 def intrams_checker(e):
     document.getElementById('output').innerHTML = ' '
     document.getElementById('image').innerHTML = ' '
-    
-    registration = document.querySelector('input[name="registration"]:checked').value
-    clearance = document.querySelector('input[name="clearance"]:checked').value
+
+    registration_el = document.querySelector('input[name="registration"]:checked')
+    clearance_el = document.querySelector('input[name="clearance"]:checked')
+
+    if not registration_el or not clearance_el:
+        display("Please answer all the questions.", target="output")
+        return
+
+    registration = registration_el.value
+    clearance = clearance_el.value
     grade_level = int(document.getElementById('level').value)
     section = document.getElementById('section').value
 
     if registration != 'registered':
-        display(f'Not eligible: student is not registered for Intrams. Ask your PE teacher regarding the online registraton.', target='output')
+        display('Not eligible: student is not registered for Intrams.', target='output')
+
     elif clearance != 'cleared':
-        display(f'Not eligible: medical clearance required. Go to the clinic and secure your clearance.', target='output')
+        display('Not eligible: medical clearance required.', target='output')
+
     elif section == 'emerald':
-        display(f'Congratulations! You are part of the ...', target='output')
+        display('Congratulations! You are part of the ...', target='output')
         document.getElementById("image").innerHTML = "<img src='Green.png' width='300'>"
+
     elif section == 'ruby':
-        display(f'Congratulations! You are part of the ...', target='output')
+        display('Congratulations! You are part of the ...', target='output')
         document.getElementById("image").innerHTML = "<img src='Red.png' width='300'>"
+
     elif section == 'sapphire':
-        display(f'Congratulations! You are part of the ...', target='output')
+        display('Congratulations! You are part of the ...', target='output')
         document.getElementById("image").innerHTML = "<img src='Blue.png' width='300'>"
-    else: 
-        display(f'Congratulations! You are part of the ...', target='output')
+
+    else:
+        display('Congratulations! You are part of the ...', target='output')
         document.getElementById("image").innerHTML = "<img src='Yellow.png' width='300'>"
